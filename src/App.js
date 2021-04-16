99bcfd06bbaf372ccd201dec1d458ea6f9b8d151
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-04-16 22:36:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../src/Component/Home/Home";
+import Nomatch from "./Component/NoMatch/Nomatch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="*">
+          <Nomatch></Nomatch>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Component/Home/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            this is nav background_color
+        </div>
+    );
+};
+
+export default Navbar;
