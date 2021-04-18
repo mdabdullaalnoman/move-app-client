fe01644eb2aeb20794a2eededd67af914e340586
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-04-19 05:32:28 +0600

    done

--- src/Component/Home/Navbar.js
+++ src/Component/Home/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Navbar.css';
+import logo from '../../images/logo.png';
 
 const Navbar = () => {
     return (
-        <div>
-            this is nav background_color
+        <div className="nav-bg">
+            <nav className="container main-nav">
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                </div>
+
+                <div className="menu" style={{display: 'flex', justifyContent: 'center'}}>
+                    <ul>
+                        <li><a href="/home">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/details">Details</a></li>
+                        <li><a href="move">Move</a></li>
+                        <li><a href="move"><button>Login</button></a></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 };
--- src/Component/NoMatch/Nomatch.js
+++ src/Component/NoMatch/Nomatch.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+
 const Nomatch = () => {
     return (
         <div>
             <h1>No match 404</h1>
+            <h2>404</h2>
         </div>
     );
 };
